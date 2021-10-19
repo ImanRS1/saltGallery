8d76bccee7b5158186dca4466d4b204485d4096e
salt-dev <salt-dev@DEV014.local>
2021-10-19 12:50:43 +0200

    API search function implemented

--- api.js
+++ api.js
@@ -1,13 +1,33 @@
 const config = require('./config');
 
-const apiFetch = fetch(`https://api.unsplash.com/photos/?client_id=${config.key}`)
-  .then(img => img.json())
-  .then(img => img[0])
-  .then(img => img.urls)
-  .then(img => img.raw)
-  .then(img => {
-    const target = document.querySelector('.image');
-    target.innerHTML = `<img src="${img}" class="target-img">`;
-  });
+const apiSearch = (query, page=1) => {
+  fetch(`https://api.unsplash.com/search/photos/?query=${query}&page=${page}&per_page=9&client_id=${config.key}`)
+    .then(imgs => imgs.json())
+    .then(imgs => {
+      const totalPages = imgs.total_pages;
+      const imgArr = imgs.results;
+      const target = document.querySelector('.image');
+      if (imgs.total === 0) {
+        return target.innerHTML = '<p class="no-results">Sorry, there were no results to your search!</p>'
+      };
+      let html = '';
+      imgArr.forEach(img => {
+        html += `<img src="${img.urls.small}" class="target-img">`;
+      });
+      target.innerHTML = html;
+      if (page < totalPages) {
+        document.querySelector(".nextBtn").classList.remove("hidden");
+      };
+      if (page > 1) {
+        document.querySelector(".prevBtn").classList.remove("hidden");
+      };
+      if (page === 1) {
+        document.querySelector(".prevBtn").classList.add("hidden");
+      }
+      if (page === totalPages) {
+        document.querySelector(".nextBtn").classList.add("hidden");
+      }
+    })
+};
 
-module.exports = apiFetch;
+module.exports = { apiSearch };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/main.scss';
 
-const apiFetch = require('../api');
+const { apiSearch } = require('../api');
 
 // Create heading node
 const header = document.createElement('header');
@@ -13,15 +13,79 @@ footer.innerHTML = '<a href="https://www.github.com" class="page-footer__github-
 
 const searchBar = document.createElement('div');
 searchBar.className = 'search-bar';
-searchBar.innerHTML = '<input type="text" placeholder="SEARCH" class="search-bar__search-input">';
+searchBar.innerHTML = '<form><input type="text" placeholder="SEARCH" class="search-bar__search-input"><button type="submit" class="submitBtn">Search</button></form>';
 
 const imageContainer = document.createElement('section');
 imageContainer.className = 'image-container';
-imageContainer.innerHTML = '<div class="image"></div>';
+imageContainer.innerHTML = '<div class="image"></div><div class="buttons"><button class="prevBtn hidden">Previous</button><button class="nextBtn hidden">Next</button></div>';
+
+const dropDown = document.createElement('ul');
+dropDown.className = 'list-group';
 
 // Append heading node to the DOM
 const app = document.querySelector('#root');
 app.append(header);
 app.append(searchBar);
+app.append(dropDown);
 app.append(imageContainer);
 app.append(footer);
+
+const ul = document.querySelector('.list-group');
+let recentSearches;
+
+if (localStorage.recentSearches && localStorage.recentSearches != "") {
+  recentSearches = JSON.parse(localStorage.recentSearches);
+} else {
+  recentSearches = [];
+}
+
+const isDuplicateValue = (arr, text) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == text) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const makeListItem = (text, parent) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = text;
+    listItem.className = "list-group-item";
+    parent.appendChild(listItem);
+  };
+
+recentSearches.forEach(element => {
+  makeListItem(element, ul);
+});
+
+document.querySelector('.submitBtn').addEventListener('click', e => {
+  e.preventDefault();
+  apiSearch(document.querySelector('.search-bar__search-input').value);
+  const searchBar = document.querySelector('.search-bar__search-input');
+  if (
+    searchBar.value == "" ||
+    isDuplicateValue(recentSearches, searchBar.value)
+  ) {
+    return;
+  } else {
+    recentSearches.push(searchBar.value);
+    makeListItem(searchBar.value, ul);
+    localStorage.recentSearches = JSON.stringify(recentSearches);
+    searchBar.value = "";
+  }
+});
+
+let page = 1;
+
+document.querySelector('.nextBtn').addEventListener('click', e => {
+  e.preventDefault();
+  page += 1;
+  apiSearch(document.querySelector('.search-bar__search-input').value, page);
+});
+
+document.querySelector('.prevBtn').addEventListener('click', e => {
+  e.preventDefault();
+  page -= 1;
+  apiSearch(document.querySelector('.search-bar__search-input').value, page);
+});
