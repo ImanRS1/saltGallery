f356fd0c93abcbb9d3a1c99dcbada8a0d118afe8
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2021-10-20 14:52:08 +0200

    test update

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 const { addNumbers } = require('./api.js');
 
-test('adds two numbers', () => {
+test('adds two numbers', ( ) => {
   console.log('sumtest ran');
   expect(addNumbers(1, 2)).toBe(3);
 });
